731f93fc51afe0210a3b1db01b5cfbec5e74b7a6
Joseph Corrado <josephcorrado7@gmail.com>
2021-12-21 00:48:19 -0500

    Needed to reference the .name attribute of the self.color objects, because they're actually ItemEnums, not strings.

--- producers/models/station.py
+++ producers/models/station.py
@@ -55,19 +55,26 @@ class Station(Producer):
         Simulates train arrivals at this station.
         Station.run() should emit an arrival event to Kafka
         """
-        self.producer.produce(
-           topic=self.topic_name,
-           key={"timestamp": Producer.time_now_in_millis()},
-           value={
-               "station_id": self.station_id,
-               "train_id": train.train_id,
-               "direction": direction,
-               "line": self.color,
-               "train_status": train.status,
-               "prev_station_id": prev_station_id,
-               "prev_direction": prev_direction
-           },
-        )
+        data = {
+            "station_id": str(self.station_id),
+            "train_id": train.train_id,
+            "direction": direction,
+            "line": self.color.name,
+            "train_status": train.status.name,
+            "prev_station_id": prev_station_id,
+            "prev_direction": prev_direction
+        }
+        try:
+            self.producer.produce(
+               topic=self.topic_name,
+               key={"timestamp": Producer.time_now_in_millis()},
+               value=data
+            )
+        except AttributeError as e:
+            print("Received an attribute error.")
+            print("Data:\n")
+            print(data)
+            raise AttributeError
 
     def __str__(self):
         return "Station | {:^5} | {:<30} | Direction A: | {:^5} | departing to {:<30} | Direction B: | {:^5} | departing to {:<30} | ".format(
