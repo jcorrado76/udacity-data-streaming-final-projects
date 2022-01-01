c37e1ef153c99a7ae69aea1b53f8d87fe160edc9
Joseph Corrado <josephcorrado7@gmail.com>
2021-12-31 21:47:59 -0500

    Added the work on the pyspark streaming script which loads data from the kafka topic containing data from the redis kafka connector, and outputs the static customer data and birthday information

--- sparkpyrediskafkastreamtoconsole.py
+++ sparkpyrediskafkastreamtoconsole.py
@@ -2,96 +2,142 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, to_json, col, unbase64, base64, split, expr
 from pyspark.sql.types import StructField, StructType, StringType, BooleanType, ArrayType, DateType
 
-# TO-DO: create a StructType for the Kafka redis-server topic which has all changes made to Redis - before Spark 3.0.0, schema inference is not automatic
+# create a StructType for the Kafka redis-server topic which has all changes made to Redis
+redisMessageSchema = StructType([
+    StructField("key", StringType()),
+    StructField("value", StringType()),
+    StructField("expiredType", StringType()),
+    StructField("expiredValue", StringType()),
+    StructField("existType", StringType()),
+    StructField("ch", StringType()),
+    StructField("incr", BooleanType()),
+    StructField("zSetEntries", ArrayType(StructType([
+        StructField("element", StringType()),
+        StructField("score", StringType())
+    ])))
+])
 
-# TO-DO: create a StructType for the Customer JSON that comes from Redis- before Spark 3.0.0, schema inference is not automatic
+# create a StructType for the Customer JSON that comes from Redis
+customerJSONSchema = StructType([
+    StructField("customerName", StringType()),
+    StructField("email", StringType()),
+    StructField("phone", StringType()),
+    StructField("birthDay", StringType()),
+])
 
-# TO-DO: create a StructType for the Kafka stedi-events topic which has the Customer Risk JSON that comes from Redis- before Spark 3.0.0, schema inference is not automatic
+# create a spark application object
+spark = SparkSession.builder.appName("customer-birthday").getOrCreate()
+# set the spark log level to WARN
+spark.sparkContext.setLogLevel('WARN')
 
-#TO-DO: create a spark application object
+# using the spark application object, read a streaming dataframe from the Kafka topic redis-server as the source
+# set startingOffsets to earliest - this includes all events from the topic, including those that were
+# published before you started the spark stream
+redisServerRawStreamingDF = spark \
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("subscribe", "redis-server") \
+    .option("startingOffsets", "earliest") \
+    .load()
 
-#TO-DO: set the spark log level to WARN
+# cast the value column in the streaming dataframe as a STRING
+redisServerStreamingDF = redisServerRawStreamingDF.selectExpr("cast(key as string) key", "cast(value as string) value")
 
-# TO-DO: using the spark application object, read a streaming dataframe from the Kafka topic redis-server as the source
-# Be sure to specify the option that reads all the events from the topic including those that were published before you started the spark stream
+"""
+The redisServerStreamingDF dataframe has a column named "value", whose entries are JSON-like strings. They look like this:
++------------+
+| value      |
++------------+
+|{"key":"Q3..|
++------------+
 
-# TO-DO: cast the value column in the streaming dataframe as a STRING 
+Those JSON objects have the following format:
+{
+  "key":"Q3VzdG9tZXI=",
+  "existType":"NONE",
+  "Ch":false,
+  "Incr":false,
+  "zSetEntries":[{
+  "element":"eyJjdXN0b21lck5hbWUiOiJTYW0gVGVzdCIsImVtYWlsIjoic2FtLnRlc3RAdGVzdC5jb20iLCJwaG9uZSI6IjgwMTU1NTEyMTIiLCJiaXJ0aERheSI6IjIwMDEtMDEtMDMifQ==",
+  "Score":0.0
+  }],
+  "zsetEntries":[{
+  "element":"eyJjdXN0b21lck5hbWUiOiJTYW0gVGVzdCIsImVtYWlsIjoic2FtLnRlc3RAdGVzdC5jb20iLCJwaG9uZSI6IjgwMTU1NTEyMTIiLCJiaXJ0aERheSI6IjIwMDEtMDEtMDMifQ==",
+  "score":0.0
+  }]
+}
 
-# TO-DO:; parse the single column "value" with a json object in it, like this:
-# +------------+
-# | value      |
-# +------------+
-# |{"key":"Q3..|
-# +------------+
-#
-# with this JSON format: {"key":"Q3VzdG9tZXI=",
-# "existType":"NONE",
-# "Ch":false,
-# "Incr":false,
-# "zSetEntries":[{
-# "element":"eyJjdXN0b21lck5hbWUiOiJTYW0gVGVzdCIsImVtYWlsIjoic2FtLnRlc3RAdGVzdC5jb20iLCJwaG9uZSI6IjgwMTU1NTEyMTIiLCJiaXJ0aERheSI6IjIwMDEtMDEtMDMifQ==",
-# "Score":0.0
-# }],
-# "zsetEntries":[{
-# "element":"eyJjdXN0b21lck5hbWUiOiJTYW0gVGVzdCIsImVtYWlsIjoic2FtLnRlc3RAdGVzdC5jb20iLCJwaG9uZSI6IjgwMTU1NTEyMTIiLCJiaXJ0aERheSI6IjIwMDEtMDEtMDMifQ==",
-# "score":0.0
-# }]
-# }
-# 
-# (Note: The Redis Source for Kafka has redundant fields zSetEntries and zsetentries, only one should be parsed)
-#
-# and create separated fields like this:
-# +------------+-----+-----------+------------+---------+-----+-----+-----------------+
-# |         key|value|expiredType|expiredValue|existType|   ch| incr|      zSetEntries|
-# +------------+-----+-----------+------------+---------+-----+-----+-----------------+
-# |U29ydGVkU2V0| null|       null|        null|     NONE|false|false|[[dGVzdDI=, 0.0]]|
-# +------------+-----+-----------+------------+---------+-----+-----+-----------------+
-#
-# storing them in a temporary view called RedisSortedSet
+(Note: The Redis Source for Kafka has redundant fields zSetEntries and zsetentries, only one should be parsed)
 
-# TO-DO: execute a sql statement against a temporary view, which statement takes the element field from the 0th element in the array of structs and create a column called encodedCustomer
-# the reason we do it this way is that the syntax available select against a view is different than a dataframe, and it makes it easy to select the nth element of an array in a sql column
+Parse those JSON fields out into separte columns, having the following schema:
++------------+-----+-----------+------------+---------+-----+-----+-----------------+
+|         key|value|expiredType|expiredValue|existType|   ch| incr|      zSetEntries|
++------------+-----+-----------+------------+---------+-----+-----+-----------------+
+|U29ydGVkU2V0| null|       null|        null|     NONE|false|false|[[dGVzdDI=, 0.0]]|
++------------+-----+-----------+------------+---------+-----+-----+-----------------+
 
-# TO-DO: take the encodedCustomer column which is base64 encoded at first like this:
-# +--------------------+
-# |            customer|
-# +--------------------+
-# |[7B 22 73 74 61 7...|
-# +--------------------+
+Then store them in a temporary view called RedisSortedSet
+"""
+redisServerStreamingDF.withColumn("value", from_json("value", redisMessageSchema)) \
+    .select(
+    col("key"),
+    col("value"),
+    col('value.existType'),
+    col('value.Ch'),
+    col('value.Incr'),
+    col('value.zSetEntries'),
+) \
+    .createOrReplaceTempView("RedisSortedSet")
 
-# and convert it to clear json like this:
-# +--------------------+
-# |            customer|
-# +--------------------+
-# |{"customerName":"...|
-#+--------------------+
-#
-# with this JSON format: {"customerName":"Sam Test","email":"sam.test@test.com","phone":"8015551212","birthDay":"2001-01-03"}
+# execute a sql statement against a temporary view, which statement takes the "element" key from the 0th element
+# in the arrays of the zSetEntries column and create a column called encodedCustomer
+# it's easier to select the nth element of rows that have arrays in them using SparkSQL syntax
+zSetEntriesEncodedStreamingDF = spark.sql("select key, zSetEntries[0].element as encodedCustomer from RedisSortedSet")
 
-# TO-DO: parse the JSON in the Customer record and store in a temporary view called CustomerRecords
+# Create a new column, called decodedCustomer that is the base64 DE-CODED version of the encodedCustomer column, then
+# cast it as a string
+zSetDecodedEntriesStreamingDF = zSetEntriesEncodedStreamingDF.withColumn("decodedCustomer", unbase64(
+    zSetEntriesEncodedStreamingDF.encodedCustomer).cast("string"))
 
-# TO-DO: JSON parsing will set non-existent fields to null, so let's select just the fields we want, where they are not null as a new dataframe called emailAndBirthDayStreamingDF
+# parse the JSON-like strings in decodedCustomer into separate columns
+# and create a temporary view called CustomerRecords
+zSetDecodedEntriesStreamingDF \
+    .withColumn("customer", from_json("decodedCustomer", customerJSONSchema)) \
+    .select(col("customer.*")) \
+    .createOrReplaceTempView("CustomerRecords")
 
-# TO-DO: from the emailAndBirthDayStreamingDF dataframe select the email and the birth year (using the split function)
+# create a dataframe selecting emails and birthdays from the CustomerRecords table
+# where email and birthday are not null
+emailAndBirthDayStreamingDF = spark.sql(
+    "SELECT email, birthDay FROM CustomerRecords WHERE email IS NOT NULL AND birthDay IS NOT NULL"
+)
 
-# TO-DO: Split the birth year as a separate field from the birthday
-# TO-DO: Select only the birth year and email fields as a new streaming data frame called emailAndBirthYearStreamingDF
+# create a dataframe containing just the birth YEAR (using the split function) and the customers email
+# then sink that stream to console output in append mode
+emailAndBirthYearStreamingDF = emailAndBirthDayStreamingDF.select(
+    "email",
+    split(
+        emailAndBirthDayStreamingDF.birthDay,
+        "-"
+    ).getItem(0).alias("birthYear")) \
+    .writeStream.outputMode("append").format("console").start().awaitTermination()
 
-# TO-DO: sink the emailAndBirthYearStreamingDF dataframe to the console in append mode
-# 
-# The output should look like this:
-# +--------------------+-----               
-# | email         |birthYear|
-# +--------------------+-----
-# |Gail.Spencer@test...|1963|
-# |Craig.Lincoln@tes...|1962|
-# |  Edward.Wu@test.com|1961|
-# |Santosh.Phillips@...|1960|
-# |Sarah.Lincoln@tes...|1959|
-# |Sean.Howard@test.com|1958|
-# |Sarah.Clark@test.com|1957|
-# +--------------------+-----
+"""
+The output should look like this:
++--------------------+-----               
+| email         |birthYear|
++--------------------+-----
+|Gail.Spencer@test...|1963|
+|Craig.Lincoln@tes...|1962|
+|  Edward.Wu@test.com|1961|
+|Santosh.Phillips@...|1960|
+|Sarah.Lincoln@tes...|1959|
+|Sean.Howard@test.com|1958|
+|Sarah.Clark@test.com|1957|
++--------------------+-----
 
-# Run the python script by running the command from the terminal:
-# /home/workspace/submit-redis-kafka-streaming.sh
-# Verify the data looks correct 
+Run the python script by running the command from the terminal:
+/home/workspace/submit-redis-kafka-streaming.sh
+Verify the data looks correct
+"""
